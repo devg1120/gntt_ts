7b604e9e5fdc52e5ccd2d6aed902dd3fb03a8bd8
西草昌市 <devg1120@gmail.com>
2026-04-06 22:48:18 +0900

    up

---
+++ src/components/COPY.sh
@@ -0,0 +1,26 @@
+echo copy to [$1]
+echo y/n
+read input 
+
+if [ -z $input ] ; then
+   echo cancel!!
+   exit 1
+
+elif [ $input = 'yes' ] || [ $input = 'YES' ] || [ $input = 'y' ] ; then
+   echo ""
+
+else
+   echo cancel!!
+   exit 1
+fi
+
+echo $1 "copy"
+
+if [ -f  ../../../gntt_org2/src/components/$1 ] ; then
+
+   echo exist  ../../../gntt_org2/src/components/$1
+   #cp ../../../gntt_org2/src/components/$1
+else
+   echo not exist  ../../../gntt_org2/src/components/$1
+    
+fi
