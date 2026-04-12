9bf025f42222d4a3fba48bf837a9cdd24d341861
devg1120 <devg1120@gmail.com>
2026-04-12 15:47:03 +0900

    up

---
+++ check2.sh
@@ -0,0 +1,21 @@
+ 
+P () {
+  echo === $*
+}
+
+
+P -----------------------
+P ArrowLayer
+./tcheck.sh src/components/ArrowLayer.tsx
+# P ArrowLayerBatched
+#./tcheck.sh src/components/ArrowLayerBatched.tsx
+#./tcheck.sh src/components/DateHeaders.tsx
+#./tcheck.sh src/components/ExpandedTaskContainer.tsx
+#./tcheck.sh src/components/Gantt.tsx
+#./tcheck.sh src/components/GanttContainer.tsx
+#./tcheck.sh src/components/Grid.tsx
+#./tcheck.sh src/components/ResourceColumn.tsx
+#./tcheck.sh src/components/SubtaskBar.tsx
+#./tcheck.sh src/components/SummaryBar.tsx
+#./tcheck.sh src/components/TaskLayer.tsx
+
